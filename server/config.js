18c0b08728438b1031f31a1fc1f2acdd190a26cc
Akshay <raymntechnologies@gmail.com>
2018-09-16 21:51:07 -0400

    initial Changes

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+   'serverport':1978,
+   'tokenexp': 3600,
+   'secret': 'mysecretkey',
+  // 'database': 'mongodb://localhost:27017/expensedb'//add mlab url
+  //'database': 'ds261450.mlab.com:61450/expensedb'//add mlab url
+  'database':'mongodb://admin:admin1234@ds261450.mlab.com:61450/expensedb'
+};
